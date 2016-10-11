bb6a46b0fc3fc8254fbde1e8046d75b68e24bfc4
Raghav <author.raghav@gmail.com>
2016-10-11 05:34:19 +0530

    added helworld proj

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		printHello();
+	}
+	
+	public static void printHello(){
+		System.out.println("Hello World...");
+	}
+}
